a518a78b9726fb03993ed1b250c1418c97982d82
TechSmith90210 <sshaikhdev@gmail.com>
2024-09-22 02:56:15 +0530

    added business login screen

---
+++ lib/screens/business_login_screen.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class BusinessLoginScreen extends StatelessWidget {
+  const BusinessLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.all(30),
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            Text(
+              'Business Login',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Access Your Seller Dashboard',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 15),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Form(
+                child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Email',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                    keyboardType: TextInputType.emailAddress,
+                  )
+                ],
+              ),
+            )),
+            SizedBox(
+              height: 25,
+            ),
+            Form(
+                child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Password',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )),
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  minimumSize: const Size(400, 65),
+                ),
+                child: const Text("Login",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19,
+                        fontWeight: FontWeight.w600)),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 35,
+                  width: 235,
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                        style: TextStyle(color: Colors.grey, fontSize: 13),
+                        children: [
+                          TextSpan(
+                            text: 'Haven’t Registered your Business Yet?',
+                          ),
+                          TextSpan(
+                              text: 'Click to Register',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.black,
+                                decoration: TextDecoration.underline,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {}),
+                        ]),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
