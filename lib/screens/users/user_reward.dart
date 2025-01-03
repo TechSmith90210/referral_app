d65c97a3912234c39204b0b01e0eadec4ad99482
Shruti211431 <shruti.211431.it@mhssce.ac.in>
2025-01-03 20:46:46 +0530

    added rewards screen

--- lib/screens/users/user_reward.dart
+++ lib/screens/users/user_reward.dart
@@ -5,6 +5,62 @@ class UserReward extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("User Reward");
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.only(top: 70),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                        borderRadius: BorderRadius.circular(30)),
+                    hintText: 'Search',
+                    hintStyle: TextStyle(color: Colors.grey)),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                "List of Places",
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15),
+              child: ListView.separated(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.black, width: 1.3),
+                        borderRadius: BorderRadius.circular(5)),
+                    leading: CircleAvatar(),
+                    title: Text(
+                      "name",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Text("rewards"),
+                  );
+                },
+                itemCount: 15,
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(
+                    height: 10,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
